f6b785782630cdc6d425babd117ca42b4125938d
gwongilnam <nimpa3201@naver.com>
2025-07-20 07:09:05 +0900

    feat: 상품 목록

--- src/main/java/com/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/com/jpabook/jpashop/controller/ItemController.java
@@ -1,12 +1,15 @@
 package com.jpabook.jpashop.controller;
 
 import com.jpabook.jpashop.domain.item.Book;
+import com.jpabook.jpashop.domain.item.Item;
 import com.jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,12 +19,12 @@ public class ItemController {
 
     @GetMapping("/items/new")
     public String createForm(Model model) {
-        model.addAttribute("form",new BookForm());
+        model.addAttribute("form", new BookForm());
         return "items/createItemForm";
     }
 
     @PostMapping("/items/new")
-    public String create(BookForm form){
+    public String create(BookForm form) {
         Book book = new Book();
         book.setName(form.getName());
         book.setPrice(form.getPrice());
@@ -34,5 +37,13 @@ public class ItemController {
         return "redirect:/";
 
     }
+
+    @GetMapping("/items")
+    public String list(Model model){
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items",items);
+        return "items/itemList";
+    }
+
 
 }
