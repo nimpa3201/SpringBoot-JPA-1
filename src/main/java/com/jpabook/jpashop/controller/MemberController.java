a5c3ab7af9fbb421963ffd0e0f54a70eb3ea15ad
gwongilnam <nimpa3201@naver.com>
2025-07-20 06:44:38 +0900

    feat: 회원 목록 조회

--- src/main/java/com/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/com/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -42,4 +44,12 @@ public class MemberController {
         return "redirect:/";
 
     }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
+    }
+
 }
