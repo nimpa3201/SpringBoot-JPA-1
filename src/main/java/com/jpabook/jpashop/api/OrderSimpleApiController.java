25c2ac376e16c5ac2bbb981fc8da4168af44f80e
gwongilnam <nimpa3201@naver.com>
2025-07-23 07:10:19 +0900

    feat: 간단한 주문 조회 V1 - 엔티티를 직접 노출

---
+++ src/main/java/com/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,38 @@
+package com.jpabook.jpashop.api;
+
+import com.jpabook.jpashop.domain.Address;
+import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.domain.OrderStatus;
+import com.jpabook.jpashop.repository.OrderRepository;
+import com.jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * xToONE (ManyToOne, OneToOne)
+ * ORDER
+ * ORDER -> MEMBER
+ * ORDER -> DELIVERY
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); // Lazy 강제 초기화
+            order.getDelivery().getAddress(); // Lazy 강제 초기화
+        }
+        return all;
+    }
+
+}
