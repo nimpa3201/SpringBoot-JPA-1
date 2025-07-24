3d6b82c4eb81272532d66f35635e046b03bdf25a
gwongilnam <nimpa3201@naver.com>
2025-07-24 11:47:21 +0900

    feat: 주문 조회 V1 - 엔티티 직접 노출

---
+++ src/main/java/com/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,35 @@
+package com.jpabook.jpashop.api;
+
+
+import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.domain.OrderItem;
+import com.jpabook.jpashop.repository.OrderRepository;
+import com.jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o-> o.getItem().getName());
+
+        }
+        return all;
+
+    }
+
+}
