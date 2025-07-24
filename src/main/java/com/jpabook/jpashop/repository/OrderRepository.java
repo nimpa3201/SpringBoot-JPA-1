497885becc297054c53af762e70e06160019c6eb
gwongilnam <nimpa3201@naver.com>
2025-07-25 07:05:27 +0900

    feat: 주문 조회 V3 - 엔티티를 DTO로 변환 (페치 조인 최적화)

--- src/main/java/com/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/com/jpabook/jpashop/repository/OrderRepository.java
@@ -98,4 +98,13 @@ public class OrderRepository {
             .getResultList();
     }
 
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+            "select distinct o from Order o" +  // distinct id 중복 제 , 쿼리 한번으로 조회 가능
+             " join fetch o.member m" +
+              " join fetch o.delivery d"+
+              " join fetch o.orderItems oi" +
+            " join fetch oi.item i " ,Order.class)
+            .getResultList();
+    }
 }
